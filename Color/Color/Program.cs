cb754a33ad89d0a01a1bef025bc37c906d58888d
eternal <1581618775@qq.com>
2019-03-05 15:38:02 +0800

    finished helloWorld123

---
+++ Color/Color/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("Hello World!");
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+    }
+}
