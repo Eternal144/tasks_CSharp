b434d9ad9ff024ecc36352633a7728927337b9e7
eternal <1581618775@qq.com>
2019-03-14 17:56:47 +0800

    my QrCode

---
+++ HelloWorld/WindowsFormsHello/WindowsFormsHello/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsHello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(454, 380);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "btnSave";
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(388, 285);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "二维";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(405, 181);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 36);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Hello,World!";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ QrCode1/QrCode1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using Gma.QrCodeNet.Encoding;
+using System.IO;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+namespace QrCodeTest
+{
+    class Program
+    {
+        //public static System.Text.Encoding GetFileEncodeType(string filename)
+        //{
+        //    System.IO.FileStream fs = new System.IO.FileStream(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+        //    System.IO.BinaryReader br = new System.IO.BinaryReader(fs);
+        //    Byte[] buffer = br.ReadBytes(2);
+        //    if (buffer[0] >= 0xEF)
+        //    {
+        //        if (buffer[0] == 0xEF && buffer[1] == 0xBB)
+        //        {
+        //            return System.Text.Encoding.UTF8;
+        //        }
+        //        else if (buffer[0] == 0xFE && buffer[1] == 0xFF)
+        //        {
+        //            return System.Text.Encoding.BigEndianUnicode;
+        //        }
+        //        else if (buffer[0] == 0xFF && buffer[1] == 0xFE)
+        //        {
+        //            return System.Text.Encoding.Unicode;
+        //        }
+        //        else
+        //        {
+        //            return System.Text.Encoding.Default;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return System.Text.Encoding.Default;
+        //    }
+        //}
+
+        //生成图片并新建文件夹，保存至文件夹内。
+        // 1-> 001
+        // 1923->192
+        public static string getString(int s) {
+            if (s <= 0) return null;
+            string str = s.ToString(); 
+            if (str.Length >= 3) //三位数直接取
+            {
+                str = str.Substring(0, 3);
+            }
+            else //加工。
+            {
+                if(str.Length == 1)
+                {
+                    str = "00" + str;
+                }
+                else
+                {
+                    str = "0" + str;
+                }
+            }
+            return str;
+        }
+        //截取前面的四位字符
+        public static string GetContSplit(string con)
+        {
+            while(con.Length < 4)
+            {
+                con += "?";
+            }
+            return con.Substring(0, 4);
+        }
+
+        private void SaveCodeFile(string content, int col_n,string path)
+        {
+            //在目标文件夹创建一个空的.bmp文件
+            string filename =path +"/"+ Program.getString(col_n) + Program.GetContSplit(content) + ".bmp";
+            
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode code = new QrCode();
+            qrEncoder.TryEncode(content, out code);
+            const int modelSizeInPixels = 4;
+
+            GraphicsRenderer render = new GraphicsRenderer(new FixedModuleSize(modelSizeInPixels, QuietZoneModules.Two)
+                , Brushes.Black, Brushes.White);
+   
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                render.WriteToStream(code.Matrix, ImageFormat.Png, fs);
+            }
+        }
+
+        public void GetQrCodePrint(string content)
+        {
+            //content被修改。
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.M);
+            QrCode qrCode = qrEncoder.Encode("content");
+
+            for (int j = 0; j < qrCode.Matrix.Width; j++)
+            {
+                for (int i = 0; i < qrCode.Matrix.Width; i++)
+                {
+
+                    char charToPrint = qrCode.Matrix[i, j] ? '　' : '■';
+                    Console.Write(charToPrint);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.WriteLine(args.Length);
+            //判断是否有命令行参数输入
+            if (args.Length  == 1)
+            {
+                string target = args[0].StartsWith("-f") ? args[0].Substring(2) : args[0];
+                //Encoding encoding = Program.GetFileEncodeType(target);
+                StreamReader srReadFile = new StreamReader(target,Encoding.UTF8);
+
+                //判断是否需要存储图片。
+                if (args[0].StartsWith("-f"))
+                {
+                    
+                    //创建一个文件夹。文件夹储存在exe所在目录下。
+                    string path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                    path = path + "pics";
+                    Console.WriteLine(path);
+                    if (Directory.Exists(path))
+                    {
+                        Directory.Delete(path,true);
+                    }
+                    Directory.CreateDirectory(path);
+                    Console.WriteLine("正在生成目录。");
+
+                    int i = 1;
+                    while (!srReadFile.EndOfStream)
+                    {
+                        Console.WriteLine("正在生成文件。");
+                        string strReadLine = srReadFile.ReadLine(); //读取每行数据
+
+                        Console.WriteLine(strReadLine);
+                        obj.SaveCodeFile(strReadLine, i, path);
+                        i++;
+                    }
+                    srReadFile.Close();
+                }
+                else
+                {
+                    int i = 1;
+                    while (!srReadFile.EndOfStream)
+                    {
+                        string strReadLine = srReadFile.ReadLine(); //读取每行数据
+                        Console.WriteLine("这是第" + i + "行信息生成的二维码");
+                        obj.GetQrCodePrint(strReadLine);
+                        i++;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.Write(@"Type some text to QR code: ");
+                string text = Console.ReadLine();
+                while (text.Length > 20)
+                {
+                    Console.WriteLine("Length too long, please input again:");
+                    text = Console.ReadLine();
+                }
+                obj.GetQrCodePrint(text);
+            }
+            Console.ReadKey();
+        }
+    }
+}
