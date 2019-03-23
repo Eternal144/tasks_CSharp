40dca8550724fab7fdecfd067e88e1360060b752
eternal <1581618775@qq.com>
2019-03-23 11:19:06 +0800

    a simple TipCalculator

---
+++ TipCalculator/TipCalculator/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TipCalculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tip tip;
+
+        public MainWindow()
+        {
+            tip = new Tip();
+            InitializeComponent();
+            
+        }
+
+        private void amountTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            billAmountTextBox.Text = tip.BillAmount;
+            //if (billAmountTextBox.Text != tip.BillAmount)
+            //{
+               
+            //}
+        }
+
+        private void billAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+       {
+
+          PerformCalculation();
+        }
+
+        private void amountTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //billAmountTextBox.Text = "";
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            PerformCalculation();
+        }
+        private void PerformCalculation()
+        {
+            var selectedRadio = myStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+
+            tip.CalculateTip(billAmountTextBox.Text, double.Parse(selectedRadio.Tag.ToString()));
+
+            amountToTipTextBlock.Text = tip.TipAmount;
+            totalTextBlock.Text = tip.TotalAmount;
+
+        }
+
+    }
+}
